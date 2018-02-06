18ff8c5e154481f6c62a3ee8c33203765687ea02
sikefeng <2300586308@qq.com>
2018-02-06 20:56:50 +0800

    Initial commit

---
+++ mylibrary2/src/main/java/com/example/library/sikefeng/mylibrary/ToastUtils.java
@@ -0,0 +1,13 @@
+package com.example.library.sikefeng.mylibrary;
+
+/**
+ * Created by Administrator on 2018/2/6.
+ */
+
+public class ToastUtils {
+
+    public static void test(){
+        System.out.println("KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK");
+    }
+
+}
