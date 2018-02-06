a9344a45bdecfedcd4b6c29573439784e831d7ea
sikefeng <2300586308@qq.com>
2018-02-06 21:15:46 +0800

    KKKK

---
+++ mylibrary2/src/main/java/com/example/library/sikefeng/mylibrary/TUtils.java
@@ -0,0 +1,13 @@
+package com.example.library.sikefeng.mylibrary;
+
+/**
+ * Created by Administrator on 2018/2/6.
+ */
+
+public class TUtils {
+
+    public String getTString(){
+        return "KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK";
+    }
+
+}
